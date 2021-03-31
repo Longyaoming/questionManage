21e0a70f1735af3c46df4565d3e55fe285745a9f
Longyaoming <597646466@qq.com>
2021-03-31 22:42:06 +0800

    style css

--- src/views/LayoutWrap/Content.vue
+++ src/views/LayoutWrap/Content.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-container>
+  <el-container class="content">
     <!--左边区域-->
     <el-aside width="200px">
         <slot name="left"></slot>
@@ -7,7 +7,8 @@
     <!--右边内容区域-->
     <el-main>
         <!--面包屑-->
-        <div class="">
+        <div class="breadWrap">
+            <i class="fa fa-reorder" style="margin-right:10px;"></i>
             <el-breadcrumb separator="/">
                 <el-breadcrumb-item>首页</el-breadcrumb-item>
                 <el-breadcrumb-item>活动管理</el-breadcrumb-item>
@@ -32,3 +33,16 @@ import { Component, Vue } from 'vue-property-decorator';
 
 export default class Content extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+.content{
+    width:100%;
+    height: calc(100% - 60px);
+    .breadWrap{
+        border-bottom: 1px #ddd solid;
+        padding-bottom: 10px;
+        display: flex;
+        align-items: center;    
+    }
+}
+</style>
